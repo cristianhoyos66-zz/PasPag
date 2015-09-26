c9f24e99942e13bbe45613280e1972f0fd07b55b
Cristian <cristianhoyos66@hotmail.com>
2015-09-26 13:02:52 -0500

    when context is loaded the object are loaded too connexes to context

---
+++ Application/PasPagViews/src/main/java/co/com/ces4/paspag/listener/BootstrapListener.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.ces4.paspag.listener;
+
+import co.com.ces4.paspagcontrollers.CuentaJpaController;
+import co.com.ces4.paspagcontrollers.PersonaJuridicaJpaController;
+import co.com.ces4.paspagcontrollers.PersonaNaturalJpaController;
+import co.com.ces4.paspagcontrollers.TipoCuentaJpaController;
+import co.com.ces4.paspagcontrollers.TransaccionJpaController;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author cristian
+ */
+public class BootstrapListener implements ServletContextListener {
+
+   @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PasPagPU");
+        
+        CuentaJpaController cuentajpa = new CuentaJpaController(emf);
+        PersonaJuridicaJpaController personaJuridicajpa = new PersonaJuridicaJpaController(emf);
+        PersonaNaturalJpaController personaNaturaljpa = new PersonaNaturalJpaController(emf);
+        TipoCuentaJpaController tipoCuentajpa = new TipoCuentaJpaController(emf);
+        TransaccionJpaController transaccionjpa = new TransaccionJpaController(emf);
+        
+        sce.getServletContext().setAttribute("entityManagerFactory", emf);
+        sce.getServletContext().setAttribute("cuentajpa", cuentajpa);
+        sce.getServletContext().setAttribute("personaJuridicajpa", personaJuridicajpa);
+        sce.getServletContext().setAttribute("personaNaturaljpa", personaNaturaljpa);
+        sce.getServletContext().setAttribute("tipoCuentajpa", tipoCuentajpa);
+        sce.getServletContext().setAttribute("transaccionjpa", transaccionjpa);
+
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
